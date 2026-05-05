cca6a98bc83af867de36dd1a02c450e4e627ea5a
harshit saini <harshitsainidkr@gmail.com>
2026-05-05 17:36:26 +0530

    Done phase 1,2 and three of frontend while also learning react bsics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,45 @@
+import { useState } from "react";
+import axios from "axios";
+
 function App() {
+  const [file, setFile] = useState(null);
+
+  const handleUpload = (e) => {
+    setFile(e.target.files[0]);
+  };
+
+  const handleSend = async () => {
+    if (!file) {
+      alert("Select a file first");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:8000/api/upload",
+        formData,
+        {
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        }
+      );
+
+      console.log("Response:", response.data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div>
-      <h1>AMOR Phase 1</h1>
-      <button>Send Data</button>
+      <h1>AMOR Phase 3</h1>
+
+      <input type="file" onChange={handleUpload} />
+      <button onClick={handleSend}>Upload Image</button>
     </div>
   );
 }
